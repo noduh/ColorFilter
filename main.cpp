668cf1275b3ee004595f28ee7ea2d385dd9e9439
Noah Jorgensen <njorgensen@byui.edu>
2026-05-23 21:58:09 -0600

    began writing the class to hold the image data

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <ranges>
+#include <print>
+
+class ImageData {
+    private:
+        char magic_number[3];
+        int width;
+        int hight;
+        short maxval;
+        std::vector<unsigned char> bytes;
+        std::ranges::stride_view<std::ranges::drop_view<std::ranges::ref_view<std::vector<unsigned char>>>> red, green, blue;
+};
 
 int main()
 {
-    std::cout << "Hello, from ColorFilter!\n";
+    std::println("hey there");
     return 0;
 }
